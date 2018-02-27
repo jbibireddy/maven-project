98200d758d3a717c7513b74c0c5da0c1c857eb9b
Jayesh Bibireddy <jbibireddy@gmail.com>
2018-02-27 11:44:36 -0600

    Added dynamic port property

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -2,13 +2,20 @@ package com.mulesoft.training;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
+import org.mule.tck.junit4.rule.DynamicPort;
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
 
+
 public class HelloMavenTest extends FunctionalTestCase {
 
+	@Rule
+	public DynamicPort myPort = new DynamicPort("http.port");
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
+    	System.out.println("\n\nTestcase-1:  Dynamic port ----------> " + myPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
